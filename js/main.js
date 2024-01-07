462dee5fa51c3cb1590350eabfada0bd30bfd084
Adam Shay <145905613+Adam19255@users.noreply.github.com>
2024-01-07 14:51:08 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,44 +7,43 @@ $(".hamburger").on("click", function (e) {
   $(".overlay").addClass("darken");
 });
 
-$(".nav-items li a, .overlay").on("click", function (e) {
-  if (windowSize() < 1023) {
-    closeNavbar();
-  }
-});
+// $(".nav-items li a, .overlay").on("click", function (e) {
+//   if (windowSize() < 1023) {
+//     closeNavbar();
+//   }
+// });
 
-$(window).on("resize", function (e) {
-  if (windowSize() > 1023) {
-    $(".nav-items").addClass("flex");
-  } else {
-    $(".nav-items").removeClass("flex");
-    $(".nav-items").hide();
-    closeNavbar();
-  }
-});
+// $(window).on("resize", function (e) {
+//   if (windowSize() > 1023) {
+//     $(".nav-items").addClass("flex");
+//   } else {
+//     $(".nav-items").removeClass("flex");
+//     $(".nav-items").hide();
+//     closeNavbar();
+//   }
+// });
 
 $("#contact-form").on("submit", function (e) {
-  $("html, body").remove();
   e.preventDefault();
   $("html").addClass("disable-scroll");
   $(".popup").fadeIn();
   $(".overlay").addClass("darken");
 });
 
-$(".popup-content a, .overlay").on("click", function (e) {
-  $(".popup").fadeOut();
-  $("html").removeClass("disable-scroll");
-  $(".overlay").removeClass("darken");
-});
+// $(".popup-content a, .overlay").on("click", function (e) {
+//   $(".popup").fadeOut();
+//   $("html").removeClass("disable-scroll");
+//   $(".overlay").removeClass("darken");
+// });
 
-$(document).on("keydown", function (e) {
-  // exit of Escape keypress
-  if (e.keyCode === 27) {
-    $(".popup").fadeOut();
-    $("html").removeClass("disable-scroll");
-    $(".overlay").removeClass("darken");
-  }
-});
+// $(document).on("keydown", function (e) {
+//   // exit of Escape keypress
+//   if (e.keyCode === 27) {
+//     $(".popup").fadeOut();
+//     $("html").removeClass("disable-scroll");
+//     $(".overlay").removeClass("darken");
+//   }
+// });
 
 function closeNavbar() {
   $("html").removeClass("disable-scroll");
